2bc0ee08b27b78d2ed3f52eeb66b132ab5440ec0
SumisuFK <dazzyfk@mail.ru>
2023-04-13 15:31:54 +0300

    sasa

--- Catalog/views.py
+++ Catalog/views.py
@@ -4,24 +4,36 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
+from django.contrib import messages
 
 def home(request):
     return render(request,'catalog/home.html')
 
 def signupuser(request):
+    
+    username = request.POST['username']
+    email = request.POST['email']
+    password1 = request.POST['password1']
+    password2 = request.POST['password2']
+     
     if request.method == 'GET':
         return render(request, 'catalog/signupuser.html', {'form': Creationform()})
     else:
-        if request.POST['password1'] == request.POST['password2']:
-            try:
-                user = User.objects.create_user(request.POST['username'], password = request.POST['password1'])
-                user.save()
-                login(request, user)
-                return redirect('home')
-            except IntegrityError:
-                return render(request, 'catalog/signupuser.html', {'form': Creationform, 'error': 'Данный логин уже используется,выберите другой.'})
+        if User.objects.filter(email = email).excists():
+            messages.error(request, 'Пользователь с таким email уже зарегистрирован')
+            return redirect('signupuser') 
         else:
-            return render(request, 'catalog/signupuser.html', {'form': Creationform, 'error': 'Пароли не совпадают'})
+            if password1 == password2:
+                try:
+                    user = User.objects.create_user(username = username, email = email ,password = password1)
+                    user.save()
+                    login(request, user)
+                    messages.success(request, 'Вы успешно зарегистрировались')
+                    return redirect('home')
+                except IntegrityError:
+                    return render(request, 'catalog/signupuser.html', {'form': Creationform, 'error': 'Данный логин уже используется,выберите другой.'})
+            else:
+                return render(request, 'catalog/signupuser.html', {'form': Creationform, 'error': 'Пароли не совпадают'})
 
 def logoutuser(request):
     if request.method == 'POST':
