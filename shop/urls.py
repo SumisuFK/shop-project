f8185b675f3219a082f3df934803c41dc1a3b6fa
SumisuFK <dazzyfk@mail.ru>
2023-06-30 19:01:06 +0300

    revive

--- shop/urls.py
+++ shop/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from Catalog import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -27,5 +29,6 @@ urlpatterns = [
 
     #Catalog
     path('', views.home, name='home'),
-
+    
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
