c5aa3db8f58fde96b7715efd793ba1292cec55dc
SumisuFK <dazzyfk@mail.ru>
2023-07-08 12:56:05 +0300

    33

--- Catalog/models.py
+++ Catalog/models.py
@@ -11,4 +11,4 @@ class Product(models.Model):
     image6 = models.ImageField(upload_to='Catalog/images/', blank=True)
     image7 = models.ImageField(upload_to='Catalog/images/', blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=0)
-    available = models.BooleanField(default=True)
+    preorder = models.BooleanField(default=True)
