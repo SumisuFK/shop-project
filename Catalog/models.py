d8944f1ccb4a30ead7e06c77fe79aa04fea06136
sumisufk <dazzyfk@mail.ru>
2023-08-27 17:41:21 +0300

    almost last

--- Catalog/models.py
+++ Catalog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Product(models.Model):
     title = models.CharField(max_length=100, verbose_name='Название')
-    description = models.CharField(max_length=250, blank=True, verbose_name='Описание')
+    description = models.CharField(max_length=2000, blank=True, verbose_name='Описание')
     price = models.DecimalField(max_digits=10, decimal_places=0, verbose_name='Цена')
     preorder = models.BooleanField(default=True, verbose_name='Предзаказ')
     size = models.CharField(max_length=10, choices=(('S', 'S'), ('M', 'M'), ('L', 'L'), ('XL', 'XL')), default='S')
